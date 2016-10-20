6d07819f0138eb6af1d146dd7da29b6cd25717c8
Pramod A G <pramodh.ahanya@gmail.com>
2016-10-20 02:00:24 +0200

    Prime numers final commit

--- JavaAssignments/src/exercise3/PrimeNumbers.java
+++ JavaAssignments/src/exercise3/PrimeNumbers.java
@@ -5,17 +5,18 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
 
-public class PrimeNumbersV1 {
+public class PrimeNumbers {
 
 	public static void main(String[] args) {
 
-		//Version1
+		// Version1
 		// Initialization
 		int max = 0;
 		Scanner in = new Scanner(System.in);
 		boolean[] a;
 		int n = 1;
 		String primeFileName = "primeNumbers.txt";
+
 		// Reading user input for max value
 		System.out.println("Please enter number up to which prime numbers needs to be calculated.");
 		try {
@@ -25,13 +26,13 @@ public class PrimeNumbersV1 {
 		}
 		in.close();
 		a = new boolean[max + 1];
-		//
+
 		// Initializing all array elements to true
 		for (int i = 2; i <= max; i++)
 			a[i] = true;
-		// Calculating prime numbers
 
-		System.out.println("Prime numbers from 0 to " + max);
+		// Calculating prime numbers
+		System.out.println("Prime numbers from 0 to " + max + " :");
 		for (int i = 2; i <= max;) {
 			// Adding counter for a new line after 10 numbers.
 			if (n % 10 == 0)
@@ -46,14 +47,14 @@ public class PrimeNumbersV1 {
 			while (i <= max && !a[i])
 				i++;
 		}
+		System.out.println();
 		// Printing prime numbers to file.
-
 		File primeNumbersFile = new File(primeFileName);
-		BufferedWriter writer=null;
+		BufferedWriter writer = null;
 		try {
 			if (!primeNumbersFile.exists())
 				primeNumbersFile.createNewFile();
-			 writer= new BufferedWriter(new FileWriter(primeNumbersFile));
+			writer = new BufferedWriter(new FileWriter(primeNumbersFile));
 			for (int i = 0; i < a.length; i++) {
 				if (a[i] == true) {
 					writer.write(i + " ");
@@ -62,13 +63,39 @@ public class PrimeNumbersV1 {
 			writer.close();
 		} catch (Exception e) {
 			System.out.println("Unable to write to file: " + e.getMessage());
-			
 		}
-		
-		//Version2 implementation
-		
-		
-		
+
+		// Version2 implementation
+		// copying all prime numbers to new array.
+		StringBuffer sb = new StringBuffer();
+		int sum = 0;
+		int[] primeNumbers = new int[n - 1];
+		for (int i = 2, j = 0; i < a.length; i++) {
+			if (a[i] == true) {
+				primeNumbers[j] = i;
+				j++;
+			}
+
+		}
+
+		// Printing prime numbers array and adding the prime number to string
+		// buffer and sum
+		System.out.println("Printing prime numbers array(Version 2) :");
+		n = 1;
+		for (int i : primeNumbers) {
+			if (n % 10 == 0)
+				System.out.println();
+			n++;
+			sb.append(i + " ");
+			sum += i;
+			System.out.print(i + " ");
+		}
+		System.out.println();
+
+		System.out.println("Number of prime numbers from 0 to " + (a.length - 1) + ": " + primeNumbers.length);
+		System.out.println("Min :" + primeNumbers[0]);
+		System.out.println("Max :" + primeNumbers[primeNumbers.length - 1]);
+		System.out.println("Avg :" + String.format("%.3f", (float) sum / primeNumbers.length));
 
 	}
 
